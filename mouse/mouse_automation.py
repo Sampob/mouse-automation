140cce19ec5d2a90b3fb97b607d332bf0554aedc
Sampo B <sampo.bredenberg@keemail.me>
2022-12-21 01:19:01 +0200

    Changed functions to use keyword arguments, to add array compatibility

--- mouse/mouse_automation.py
+++ mouse/mouse_automation.py
@@ -2,65 +2,89 @@ import pyautogui
 from random import randint
 
 
-def click_mouse_at(x, y, rand, mouse):
+def click_mouse_at(x=None, y=None, arr=None, rand=0, mouse='left'):
     """
     Click mouse at coordinates x, y
-    'rand' adds random variation to coordinates from -rand to rand
+    `rand` adds random variation to coordinates from -rand to rand
     Random is calculated separately to both x and y value
     'mouse' specifies mouse button to click
     E.g. click_mouse_at(100, 100, 10, 'left') left clicks mouse at random position from 90, 90 to 110, 110
+    :param arr: Array [x, y] with x and y coordinates
     :param x: horizontal position on screen
     :param y: vertical position on screen
     :param rand: position variation
     :param mouse: clicked mouse button
-    :return:
+    :return: None
     """
-    pyautogui.click((x + randint(-rand, rand), (y + randint(-rand, rand))), button=mouse)
+    if x and y:
+        pyautogui.click((x + randint(-rand, rand), (y + randint(-rand, rand))), button=mouse)
+    elif arr:
+        pyautogui.click((arr[0] + randint(-rand, rand), (arr[1] + randint(-rand, rand))), button=mouse)
+    else:
+        raise TypeError("Need x and y, or arr argument")
 
 
-def click_mouse(x, y, rand, mouse):
+def click_mouse(x=None, y=None, arr=None, rand=0, mouse='left'):
     """
     Clicks mouse from current position by x and y
-    rand adds random variation to movement
+    `rand` adds random variation to movement
     Random is calculated separately to both x and y value
     'mouse' specifies mouse button to click
     E.g. click_mouse(0, 0, 0, 'left') left clicks mouse at current location
+    :param arr: Array [x, y] with x and y coordinates
     :param x: horizontal position on screen
     :param y: verical position on screen
     :param rand: position variation
     :param mouse: clicked mouse button
     :return: None
     """
-    move_mouse(x, y, rand)
+    if x and y:
+        move_mouse(x, y, rand)
+    elif arr:
+        move_mouse(arr[0], arr[1])
+    else:
+        raise TypeError("Need x and y, or arr argument")
     pyautogui.click(button=mouse)
 
 
-def move_mouse_to(x, y, rand):
+def move_mouse_to(x=None, y=None, arr=None, rand=0):
     """
     Move mouse to coordinates x, y
-    rand adds random variation to coordinates from -rand to rand
+    `rand` adds random variation to coordinates from -rand to rand
     Random is calculated separately to both x and y value
     E.g. move_mouse_to(100, 100, 10) moves mouse to a random position from 90, 90 to 110, 110
+    :param arr: Array [x, y] with x and y coordinates
     :param x: horizontal position on screen
     :param y: vertical position on screen
     :param rand: position variation
     :return: None
     """
-    pyautogui.moveTo((x + randint(-rand, rand), (y + randint(-rand, rand))))
+    if x and y:
+        pyautogui.moveTo((x + randint(-rand, rand), (y + randint(-rand, rand))))
+    elif arr:
+        pyautogui.moveTo((arr[0] + randint(-rand, rand), (arr[1] + randint(-rand, rand))))
+    else:
+        raise TypeError("Need x and y, or arr argument")
 
 
-def move_mouse(x, y, rand):
+def move_mouse(x=None, y=None, arr=None, rand=0):
     """
     Move mouse from current position by x and y
-    rand adds random variation to movement
-    With move_mouse(0, 0, 5) adds a tiny random movement to current position
+    `rand` adds random variation to movement
+    E.g. move_mouse(0, 0, 5) adds a tiny random movement to current position
+    :param arr: Array [x, y] with x and y coordinates
     :param x: horizontal position on screen
     :param y: vertical position on screen
     :param rand: position variation
     :return: None
     """
     cur_x, cur_y = pyautogui.position()
-    pyautogui.moveTo((cur_x + x + randint(-rand, rand)), (cur_y + y + randint(-rand, rand)))
+    if x and y:
+        pyautogui.moveTo((cur_x + x + randint(-rand, rand)), (cur_y + y + randint(-rand, rand)))
+    elif arr:
+        pyautogui.moveTo((cur_x + arr[0] + randint(-rand, rand)), (cur_y + arr[1] + randint(-rand, rand)))
+    else:
+        raise TypeError("Need x and y, or arr argument")
 
 
 def point_to_coordinates(point):
