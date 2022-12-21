167ca720113420fa45447ded23d02146f292f249
Sampo B <sampo.bredenberg@keemail.me>
2022-12-22 00:14:15 +0200

    Saving mouse inputs to file and demonstrating script

--- controller/file_interpeting/save_mouse.py
+++ controller/file_interpeting/save_mouse.py
@@ -1,12 +1,18 @@
 from pynput import mouse, keyboard
+import time
+
+from controller.file_interpeting.save_to_file import click_to_file_format, sleep_to_file_format
 
 key_started = False
 mouse_listener = None
+time_between = 0
 
 
 def on_press(key):
     global key_started
     global mouse_listener
+    global time_between
+
     if key == keyboard.Key.esc:
         return False
     elif key == keyboard.Key.right:
@@ -17,13 +23,17 @@ def on_press(key):
             key_started = not key_started
         elif key_started:
             print("Mouse stopped")
-            mouse_listener.join()
+            mouse_listener.stop()
+            time_between = 0
             key_started = not key_started
     else:
         print(key)
+    return True
 
 
 def on_click(x, y, button, pressed):
+    global time_between
+
     if pressed:
         mouse_button = ''
         if not 4 % button.value[0]:
@@ -36,7 +46,16 @@ def on_click(x, y, button, pressed):
             mouse_button = 'middle'
             print("middle")
         else:
+            mouse_button = '?'
             print("Unknown mouse button")
+        if time_between == 0:
+            time_between = time.time()
+        else:
+            millisec = ("%.0f" % ((time.time() - time_between) * 1000))
+            time_between = time.time()
+            sleep_to_file_format('../../data/save-test', millisec, 50)
+        click_to_file_format('../../data/save-test', [x, y, 1, mouse_button])
+
     # append_to_file_format(key[0], key[1], )
 
 
--- controller/file_interpeting/save_to_file.py
+++ controller/file_interpeting/save_to_file.py
@@ -1,10 +1,14 @@
-def append_to_file_format(filename, data, rand=0):
+def click_to_file_format(filename: str, data: 'list'):
     """
     Adds lines to `filename` with [number] of line in front
     :param filename: file to append to
-    :param data: text to add
-    :param rand: random deviation to add
+    :param data: Array [x, y, rand, mouse]
     :return: None
     """
     with open(filename, "a") as f:
         f.write('CLICK,{},{},{},{}\n'.format(data[0], data[1], data[2], data[3]))
+
+
+def sleep_to_file_format(filename: str, time: int, rand: int = 0):
+    with open(filename, "a") as f:
+        f.write('SLEEP,{},{}\n'.format(time, rand))
