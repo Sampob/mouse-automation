e24ce0586e334d6f756575d7bbbe664acdf2e518
Sampo B <sampo.bredenberg@keemail.me>
2022-12-22 01:53:05 +0200

    Deleted debug files

--- test.py
+++
@@ -1,8 +0,0 @@
-from file_interpeting.readfile import *
-from image_matching.image_functions import *
-from image_matching.pixel_functions import *
-from util.save_to_file import *
-
-readfile("test.data")
-print(compare_rgb([60, 63, 65], rgb_at_mouse(), 10))
-
