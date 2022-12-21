7db5ed99571adda51e5f5fc9cf5335c14288f62b
Sampo B <sampo.bredenberg@keemail.me>
2022-12-21 21:37:09 +0200

    Action fixes

--- controller/actions/action_functions/click_action.py
+++ controller/actions/action_functions/click_action.py
@@ -7,16 +7,20 @@ class ClickAction(ActionInterface):
     def __init__(self):
         super().__init__('CLICK')
 
-    def execute_action(self, values: list[int]) -> bool:
+    def execute_action(self, values) -> bool:
+        print(values)
         """
         Click mouse at coordinates
         :param values: Array [x, y, rand, mouse]
         :return: True to continue
         """
         if len(values) == 2:
-            # click_mouse(x, y, rand, mouse)
-            click_mouse(0, 0, int(values[0]), values[1])
+            # click_mouse(x=None, y=None, arr=None, rand=0, mouse='left')
+            click_mouse(x=0, y=0, rand=int(values[0]), mouse=values[1])
+        elif len(values) == 3:
+            # click_mouse(x=None, y=None, arr=None, rand=0, mouse='left')
+            click_mouse_at(x=int(values[0]), y=int(values[1]), rand=int(values[2]))
         elif len(values) == 4:
-            # click_mouse_at(x, y, rand, mouse)
-            click_mouse_at(int(values[0]), int(values[1]), int(values[2]), values[3])
+            # click_mouse(x=None, y=None, arr=None, rand=0, mouse='left')
+            click_mouse_at(x=int(values[0]), y=int(values[1]), rand=int(values[2]), mouse=values[3])
         return True
