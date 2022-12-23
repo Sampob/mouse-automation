6714a2f038d1b6c62665e3eb11c58fd18acb3c2b
Sampo B <sampo.bredenberg@keemail.me>
2022-12-23 19:55:37 +0200

    Model controller

---
+++ controller/model_controller.py
@@ -0,0 +1,25 @@
+from model.file_interpeting.file_interpreter import FileController
+
+
+class ModelController:
+    def __init__(self):
+        self.fc = FileController()
+
+    def set_script(self, script):
+        self.fc.set_filename(script)
+
+    def execute_script(self, loop: bool = False, times: int = None):
+        if loop:
+            while True:
+                self.fc.readfile()
+        elif times and times > 0:
+            for i in range(times):
+                self.fc.readfile()
+        else:
+            print("NEG")
+
+    def test(self, *args):
+        try:
+            print(args)
+        except:
+            print("TEST")
